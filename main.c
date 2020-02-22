a5b63c08f18acdcf4aed8b7a1d2513f5bc5608bf
Jack Wiltshire <jack.william.wiltshire@gmail.com>
2020-02-22 21:12:58 +1100

    Implemented some basic timer functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <time.h>
 #include "asoundlib.h"
 
 void frite_open(snd_rawmidi_t **hin)
@@ -11,6 +12,60 @@ void frite_open(snd_rawmidi_t **hin)
     printf("Open Result: %i\n", result);
 }
 
+void print_time()
+{
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    printf("Time: %ld.%ld ", ts.tv_sec, ts.tv_nsec);
+}
+
+struct timespec get_time(void)
+{
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return ts;
+}
+
+struct timespec diff_time(struct timespec start, struct timespec end)
+{
+    struct timespec ts;
+
+    if(end.tv_nsec < start.tv_nsec)
+    {
+        ts.tv_sec = end.tv_sec-start.tv_sec-1;
+        ts.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    }
+    else
+    {
+        ts.tv_sec = end.tv_sec-start.tv_sec;
+        ts.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+
+    return ts;
+}
+
+void wait_time(int msecs)
+{
+    struct timespec start, period, delta;
+
+    //Convert msecs to timespec
+    period.tv_sec = msecs/1000;
+    period.tv_nsec = (msecs%1000) * 1000000;
+
+    start = get_time();
+
+    while(1)
+    {
+        delta = diff_time(start, get_time());
+
+        if((delta.tv_sec == period.tv_sec &&
+            delta.tv_nsec >= period.tv_nsec) ||
+           (delta.tv_sec > period.tv_sec))
+            return;
+    }
+}
+
+
 int main(void)
 {
     snd_rawmidi_t *handle_in = NULL;
@@ -18,15 +73,18 @@ int main(void)
     ssize_t size;
     int i;
 
-
     frite_open(&handle_in);
 
-    while(1) {
+    while(1)
+    {
         size = snd_rawmidi_read(handle_in,(void*)buf,256);
-        printf("size: %i, ", size);
+        print_time();
+        wait_time(1000);
+        printf("size: %i ", size);
 
         for(i=0;i<size;i++)
             printf("%hhx", buf[i]);
+
 
         printf("\n");
     }
