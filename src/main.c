57450cff0085230fae0c715ac0d364bac573d0f0
Jack Wiltshire <jack.william.wiltshire@gmail.com>
2022-10-16 11:32:30 +0800

    Basic main template for rendering

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@
 
 int main(void)
 {
-    bool frite_midi = true;
+    bool frite_midi = false;
 
     //Screen/UI
     screen_t s;
@@ -29,7 +29,7 @@ int main(void)
     shapes_t sh;
     ui_system_t uisys;
 
-    //Sound
+    // Sound
     frite_t hw;
     keyboard_t k;
 
@@ -37,46 +37,18 @@ int main(void)
     screen_init(&s, SCREEN_WIDTH, SCREEN_HEIGHT);
     input_init(&in, &s, &hw, !frite_midi);
 
-    //Sound init
-    keys_init(&k);
-    frite_open(&hw, &k.ev, frite_midi);
-
-    if(!frite_midi)
-        in.midi_ev = &k.ev;
-
     shapes_init(&sh);
     uisys_init(&uisys, &in);
-    chords_init(&in);
-
-    waveform_t *w1 = waveform_init(10, 1000);
-    int16_t buf[2205] = {0};
-
-    textbox_add(V2(10,10), V2(200,30), "");
-    gl_charset_t cset = gl_load_charset("res/OpenSans-Bold.ttf", 40, 30, 128);
 
     while(s.close == false)
     {
-
         input_update(&in);
         uisys_update(&uisys);
-        chords_update();
 
-        //Only write if space availabe in audio buffer.
-        if(snd_pcm_avail(hw.audio_out) > hw.pback_out.period_size)
-        {
-            //Zeroize buffer prior to populating with data.
-            memset(buf, 0, hw.pback_out.period_size*sizeof(uint16_t));
+        // --- ENTER RENDER LOGIC HERE ---
 
-            keys_populate_buffer(&k, buf, hw.pback_out.period_size);
-            snd_pcm_writei(hw.audio_out, buf, hw.pback_out.period_size);
-            waveform_update(w1, buf, 2205);
-
-        }
-
-        keys_draw_notes(&k, V2(400,0), &cset);
         model_uniform_color(COLOR_INIT(255, 0, 127, 255));
-        model_draw(w1->m, 0, 0, 800, 600, false);
-
         screen_swap_buffer(&s);
     }
 }
+
